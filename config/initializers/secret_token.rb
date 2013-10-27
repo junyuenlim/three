958f18fb326d9ad73d449d112b074e4822e1a925
Jun Lim <junyuen@gmail.com>
2013-10-28 00:31:48 +0800

    Initial Commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+ActiveadminTest::Application.config.secret_token = '23e4526ade744453461cde58725959739d81ee013c31aa4fcce7bec1fc1309d01aaa89061c3162d849505f22a405f163f9d8a84ad412f6b9169d3ecd4340a41e'
